db5ed76972bb028b451a936d1660406590c97adf
JeanMNP <144269695+JeanMNP@users.noreply.github.com>
2026-03-23 16:18:39 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/CV_Online/',
   plugins: [react()],
+  base: '/CV_Online/',   // 👈 igual ao nome do repo
 })
